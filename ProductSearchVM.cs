94e1b9566da71885c531efd01526b628deded291
tayenphuong <yenphuongtyp24@gmail.com>
2024-11-10 18:39:10 +0700

    Add files via upload

---
+++ ProductSearchVM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TaYenPhuong_BE.Models.ViewModel
+{
+    public class ProductSearchVM
+    {
+        public string SearchTerm { get; set; }
+
+        public decimal? MinPrice { get; set; }
+        public decimal? MaxPrice { get; set; }
+
+        public string SortOder {  get; set; }
+        public List<Product> Products { get; set; }
+
+    }
+}
